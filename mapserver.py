4551a982fe7a1c0e8843a9400802125f1e5a9398
lpemake <petteri.makela@seamk.fi>
2024-04-04 10:58:19 +0300

    a

--- mapserver.py
+++ mapserver.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, request, Response, render_template # pip install Flask
+import json
 
 app = Flask(__name__)
+
+positions = [[51.5, -0.09], [51.6, -0.10], [51.7, -0.11]]
 
 @app.route('/')
 def home():
     return render_template('maptest.html')
+
+@app.route('/api/positions')
+def get_positions():
+    # sarjallistetaan measurements-lista json-muotoon
+    s = json.dumps(positions, indent=True)
+    # tehdään vastaus oikealla tavalla
+    resp = Response(s, status=200, mimetype='application/json')
+    return(resp)    
 
 if __name__ == '__main__':
     app.run(debug=True)
